7c66badea9e5d6fb41c0d7fa18329a1ff80ad3c8
HanBuKui <1040069852@qq.com>
2023-01-13 22:54:07 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.hmdp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.entity.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author
+ * @since 2021-12-22
+ */
+public interface IUserService extends IService<User> {
+
+}
---
+++ src/main/java/com/hmdp/service/IVoucherService.java
@@ -0,0 +1,20 @@
+package com.hmdp.service;
+
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Voucher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author
+ * @since 2021-12-22
+ */
+public interface IVoucherService extends IService<Voucher> {
+
+    Result queryVoucherOfShop(Long shopId);
+
+    void addSeckillVoucher(Voucher voucher);
+}
