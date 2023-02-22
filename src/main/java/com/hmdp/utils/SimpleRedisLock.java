c0773a0d408aab5ef4cda42d954b27d25b2ce882
HanBuKui <1040069852@qq.com>
2023-02-22 23:02:10 +0800

    【优惠券秒杀 | 分布式锁 | Lua脚本确保放锁的一致性(1. 判断线程标识是否与自己一致 2.一致情况下删除该锁)】

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -36,6 +40,13 @@ public class SimpleRedisLock implements ILock{
     */
     private static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";   //线程标识前缀(区分集群下不同主机(不同JVM) 而ThreadID在jvm内部是自增生成的)
 
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("./lua/unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程标识  (UUID:区分不同JVM + 线程id：区分同一个JVM中的不同线程)
@@ -46,14 +57,35 @@ public class SimpleRedisLock implements ILock{
         return Boolean.TRUE.equals(success);   //也是hutool的 BooleanUtil.isTrue() 的实现方法
     }
 
+    /**
+    *@Description:  Redis+Lua脚本 改善分布式锁原子性问题
+    */
     @Override
     public void unlock() {
-        //获取线程标识
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //获取锁中标识并判断标识是否一致
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if(threadId.equals(id)){
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+                );
     }
+
+    /**
+    *@Description: 解决redis误删的旧方法 （ 无法保证原子性，存在线程安全问题 ）
+    */
+//    @Override
+//    public void unlock() {
+//        //获取线程标识
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//
+//        //*********************** 由于此处是分开执行的，所以可能会导致判断一致后，在删除前出现阻塞，锁失效，新线程拿锁，旧线程继续执行删掉了新线程的锁（并发问题） ***********************************************************
+//        /**  所以关键问题是确保 拿、验证、删除 的原子性操作  => redis+Lua脚本
+//        */
+//        //获取锁中标识并判断标识是否一致
+////        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+////        if(threadId.equals(id)){
+////            stringRedisTemplate.delete(KEY_PREFIX + name);
+////        }
+//        //**********************************************************************************
+//    }
 }
---
+++ src/main/resources/lua/unlock.lua
@@ -0,0 +1,6 @@
+--比较线程标识与锁中的标识是否一致
+if (redis.call('get', KEYS[1]) == ARGV[1]) then
+    --释放锁
+    return redis.call('del',KEYS[1])
+end
+return 0
